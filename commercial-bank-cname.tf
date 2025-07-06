bd9b3066644fdf52a1da88620a67536337e4050e
Thabang Modisaotsile <thabang.modisaotsile@standardbank.co.za>
2025-07-07 01:52:46 +0200

    update cname record value

--- commercial-bank-cname.tf
+++ commercial-bank-cname.tf
@@ -1,6 +1,6 @@
 resource "azurerm_dns_cname_record" "commercial_bank_frontend" {
   name                = "commercial-bank"
-  record              = "ec2-13-247-181-151.af-south-1.compute.amazonaws.com"
+  record              = "ec2-13-247-182-160.af-south-1.compute.amazonaws.com"
   
   zone_name           = data.azurerm_dns_zone.grad_projects_dns_zone.name
   resource_group_name = "the-hive"
