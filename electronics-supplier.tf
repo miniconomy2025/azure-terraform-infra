9f9e80a22bcb00e4a280a41919bcea971579ed4c
Rudolph Esterhuysen <rudolphe@bbd.co.za>
2025-07-07 18:22:59 +0200

    Fix validation records for electronics

--- electronics-supplier.tf
+++ electronics-supplier.tf
@@ -19,8 +19,8 @@ resource "azurerm_dns_cname_record" "electronics_supplier_api" {
 }
 
 resource "azurerm_dns_cname_record" "electronics_supplier_ssl_validation_frontend" {
-  name                = "_7cc62e64057e73853baa72c73b1296f0.electronics-supplier.projects.bbdgrad.com."
-  record              = "_67adb6241bb12b613437383df4012994.xlfgrmvvlj.acm-validations.aws."
+  name                = "_7cc62e64057e73853baa72c73b1296f0.electronics-supplier"
+  record              = "_67adb6241bb12b613437383df4012994.xlfgrmvvlj.acm-validations.aws"
    
   zone_name           = data.azurerm_dns_zone.grad_projects_dns_zone.name
   resource_group_name = "the-hive"
@@ -29,8 +29,8 @@ resource "azurerm_dns_cname_record" "electronics_supplier_ssl_validation_fronten
 }
 
 resource "azurerm_dns_cname_record" "electronics_supplier_ssl_validation_api" {
-  name                = "_ddc4718574de6d44eea27a744db40f7d.electronics-supplier-api.projects.bbdgrad.com."
-  record              = "_317110ee4f532eef6ed8dd0294f4b10a.xlfgrmvvlj.acm-validations.aws."
+  name                = "_ddc4718574de6d44eea27a744db40f7d.electronics-supplier-api"
+  record              = "_317110ee4f532eef6ed8dd0294f4b10a.xlfgrmvvlj.acm-validations.aws"
    
   zone_name           = data.azurerm_dns_zone.grad_projects_dns_zone.name
   resource_group_name = "the-hive"
