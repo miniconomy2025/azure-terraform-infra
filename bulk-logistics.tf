d61848fc4d4a4fcf2274c6cf5529af2768d8dc2e
Lerato Taunyane <Lerato.Taunyane@bbd.co.za>
2025-07-08 09:53:28 +0200

    bulk_logistics_2025: record fix

--- bulk-logistics.tf
+++ bulk-logistics.tf
@@ -1,6 +1,6 @@
 resource "azurerm_dns_cname_record" "bulk_logistics_web_cname" { 
   name                = "bulk-logistics" 
-  record              = "ec2-52-87-246-255.compute-1.amazonaws.com" 
+  record              = "ec2-3-210-114-177.compute-1.amazonaws.com" 
 
   zone_name           = data.azurerm_dns_zone.grad_projects_dns_zone.name 
   resource_group_name = "the-hive" 
@@ -10,7 +10,7 @@ resource "azurerm_dns_cname_record" "bulk_logistics_web_cname" {
 
 resource "azurerm_dns_cname_record" "bulk_logistics_api_cname" { 
   name                = "bulk-logistics-api" 
-  record              = "ec2-18-206-245-16.compute-1.amazonaws.com" 
+  record              = "ec2-52-7-248-137.compute-1.amazonaws.com" 
 
   zone_name           = data.azurerm_dns_zone.grad_projects_dns_zone.name 
   resource_group_name = "the-hive" 
